e3c69e56f440cfceea2d29eaf225a8a75e6b0a48
William McCausland <mccauslw@mac.com>
2019-08-29 12:32:52 -0400

    add automatic Rcpp files

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -18,32 +18,36 @@ BEGIN_RCPP
 END_RCPP
 }
 // skew_eval_cpp
-NumericVector skew_eval_cpp(double mode, NumericVector h, double mu, double omega, NumericVector z);
-RcppExport SEXP _RHESSIAN_skew_eval_cpp(SEXP modeSEXP, SEXP hSEXP, SEXP muSEXP, SEXP omegaSEXP, SEXP zSEXP) {
+NumericVector skew_eval_cpp(int n_grid_points, int code, double mode, NumericVector h, double mu, double omega, NumericVector z);
+RcppExport SEXP _RHESSIAN_skew_eval_cpp(SEXP n_grid_pointsSEXP, SEXP codeSEXP, SEXP modeSEXP, SEXP hSEXP, SEXP muSEXP, SEXP omegaSEXP, SEXP zSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< int >::type n_grid_points(n_grid_pointsSEXP);
+    Rcpp::traits::input_parameter< int >::type code(codeSEXP);
     Rcpp::traits::input_parameter< double >::type mode(modeSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type h(hSEXP);
     Rcpp::traits::input_parameter< double >::type mu(muSEXP);
     Rcpp::traits::input_parameter< double >::type omega(omegaSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type z(zSEXP);
-    rcpp_result_gen = Rcpp::wrap(skew_eval_cpp(mode, h, mu, omega, z));
+    rcpp_result_gen = Rcpp::wrap(skew_eval_cpp(n_grid_points, code, mode, h, mu, omega, z));
     return rcpp_result_gen;
 END_RCPP
 }
 // skew_draw_cpp
-NumericVector skew_draw_cpp(double mode, NumericVector h, double mu, double omega, int n_draws);
-RcppExport SEXP _RHESSIAN_skew_draw_cpp(SEXP modeSEXP, SEXP hSEXP, SEXP muSEXP, SEXP omegaSEXP, SEXP n_drawsSEXP) {
+List skew_draw_cpp(int n_grid_points, int code, double mode, NumericVector h, double mu, double omega, int n_draws);
+RcppExport SEXP _RHESSIAN_skew_draw_cpp(SEXP n_grid_pointsSEXP, SEXP codeSEXP, SEXP modeSEXP, SEXP hSEXP, SEXP muSEXP, SEXP omegaSEXP, SEXP n_drawsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< int >::type n_grid_points(n_grid_pointsSEXP);
+    Rcpp::traits::input_parameter< int >::type code(codeSEXP);
     Rcpp::traits::input_parameter< double >::type mode(modeSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type h(hSEXP);
     Rcpp::traits::input_parameter< double >::type mu(muSEXP);
     Rcpp::traits::input_parameter< double >::type omega(omegaSEXP);
     Rcpp::traits::input_parameter< int >::type n_draws(n_drawsSEXP);
-    rcpp_result_gen = Rcpp::wrap(skew_draw_cpp(mode, h, mu, omega, n_draws));
+    rcpp_result_gen = Rcpp::wrap(skew_draw_cpp(n_grid_points, code, mode, h, mu, omega, n_draws));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -76,8 +80,8 @@ END_RCPP
 
 static const R_CallMethodDef CallEntries[] = {
     {"_RHESSIAN_alias_cpp", (DL_FUNC) &_RHESSIAN_alias_cpp, 2},
-    {"_RHESSIAN_skew_eval_cpp", (DL_FUNC) &_RHESSIAN_skew_eval_cpp, 5},
-    {"_RHESSIAN_skew_draw_cpp", (DL_FUNC) &_RHESSIAN_skew_draw_cpp, 5},
+    {"_RHESSIAN_skew_eval_cpp", (DL_FUNC) &_RHESSIAN_skew_eval_cpp, 7},
+    {"_RHESSIAN_skew_draw_cpp", (DL_FUNC) &_RHESSIAN_skew_draw_cpp, 7},
     {"_RHESSIAN_spline_eval_cpp", (DL_FUNC) &_RHESSIAN_spline_eval_cpp, 3},
     {"_RHESSIAN_spline_draw_cpp", (DL_FUNC) &_RHESSIAN_spline_draw_cpp, 3},
     {NULL, NULL, 0}
